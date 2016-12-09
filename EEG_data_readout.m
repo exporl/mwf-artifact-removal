827a37c4a8904adcdad4f8a4debc0af434680fa4
Ben Somers <ben.somers@med.kuleuven.be>
2016-12-09 14:39:56 +0100

    simplified fetching data by name and artifact type + make readout into function

--- EEG_data_readout.m
+++ EEG_data_readout.m
@@ -1,25 +1,24 @@
 % Read out the EEG data corrupted with artifacts
 % 
-% INPUT: artifacted .bdf files for the 10 test subjects
-% OUTPUT: eeg data as 64xsamples, saved in the EEG_data_normal folder in
-% the format eye_blink_<name> or muscle_<name>. The working sample rate Fs 
-%(200 Hz) is also stored along with it, as well as the duration.
+% INPUT: artifacted .bdf files for the 10 test subjects, referenced by both
+% a name and the artifact type
+%
+% OUTPUT: EEG data as channels x samples, saved in the EEG_data_readout 
+% folder in the format <name>_<artifact_type>.mat. 
+%
+% Also saves the working sample rate Fs and measurement duration.
+%
 
-clear
+function EEG_data_readout(name, artifact)
 
 % Specify the desired working frequency Fs
 Fs = 200;
 
 % Specify the location of the file on the PC (make sure it's on the matlab path!)
-folder = 'C:\Users\gebruiker\Documents\KULeuven Vakantiewerk Thesispaper 1\EEG Data - Artifacts\';
+folder = 'C:\Users\gebruiker\Documents\KULeuven Doctoraat\Thesispaper 2 - GUI\EEG Data - Artifacts';
 
-name = 'steven'; %alex, anneleen, hanneke, jan-peter, jeroen, jonas, lorenz, otto, steven
 % bdf_loc = [folder name '\blink.bdf'];
-bdf_loc = [folder name '\clench.bdf'];
-
-% % One subject has no "blink" every 5 seconds, but a "blink continously" measurement
-% name = 'olivia';
-% bdf_loc = [folder name '\blink_continuously.bdf'];
+bdf_loc = [folder filesep name filesep artifactBDFname(artifact) '.bdf'];
 
 % Read out the bdf file
 eeg_struct  = biopil_raw_data('FileName',bdf_loc, 'MultipleEpochs', 'ignore','Channels','all');
@@ -31,16 +30,6 @@ duration = eeg_struct.FileHeader.RecordCount;   % Duration of measurement in sec
 % Apply high- and lowpass filtering as preprocessing
 eeg_struct = biopil_filter(eeg_struct,'HighPass', 1,'LowPass', [] );
 
-% % NOTCH FILTER IF NEEDED
-% % Apply a notch filter on 50 Hz and it's harmonics smaller than Fs
-% for i = 50:50:floor(Fs/50)*50
-%     wo = i/(Fs_original/2); bw = wo/35; % location and 3dB Bandwidth of notch filter
-%     [num,den] = iirnotch(wo,bw);
-%     eeg_struct.RawData.EegData = filter(num,den,eeg_struct.RawData.EegData);
-% end
-% clear num den wo bw i
-
-
 % Resample the EEG measurement from 8192 Hz to a lower working frequency Fs
 % Resample() already applies a LP Anti-Aliasing Filter
 eeg_struct.RawData.EegData = resample(double(eeg_struct.RawData.EegData),Fs,Fs_original);
@@ -48,50 +37,25 @@ eeg_struct.RawData.EegData = resample(double(eeg_struct.RawData.EegData),Fs,Fs_o
 % Put the data in the right format (channels x samples)
 eeg_data = (eeg_struct.RawData.EegData(:,1:64)).';
 
-% Remove unwanted wave artifacts in the case of Jeroen
-% if (strcmp(name,'jeroen'))
-%     eeg_data(:,Fs*53.5:Fs*55.5) = [];
-%     eeg_data(:,Fs*33.5:Fs*35.5) = [];
-%     eeg_data(:,Fs*11.5:Fs*14) = [];
-%     eeg_data(:,Fs*6:Fs*9) = [];
-%     duration = duration - 9.52;
-% end
-% if (strcmp(name,'alex'))
-%     eeg_data(:,Fs*86:Fs*88) = [];
-%     eeg_data(:,Fs*77.5:Fs*82) = [];
-%     eeg_data(:,Fs*63:Fs*65.5) = [];
-%     eeg_data(:,Fs*52:Fs*58) = [];
-%     duration = duration - 15.02;
-% end
-
 % Save the structure and resampled frequency to a mat-file
-save(['EEG_data_readout\muscle_' name '.mat'],'eeg_data','Fs','duration');
+save(['EEG_data_readout' filesep name '_' artifact '.mat'],'eeg_data','Fs','duration');
 
-% Make a plot of the eeg measurements
-eegplot(eeg_data,'srate',Fs,'spacing',80,'winlength',10,'dispchans',16)
 
-% Calculation of the spectrum of a channel
-channel = eeg_data(3,:);
+function [artifact_file] = artifactBDFname(artifact)
 
-Ts = 1/Fs;                  % Sample time
-L = duration*Fs;            % Length of signal in frames
-t = (0:L-1)*Ts;             % Time vector
+switch artifact
+    case 'eyeblink'
+        artifact_file = 'blink';
+    case 'muscle'
+        artifact_file = 'clench';
+    case 'speech'
+        artifact_file = 'count';
+    case 'movement'
+        artifact_file = 'nod';
+    case 'mix'
+        artifact_file = 'random';
+    otherwise
+        error('Error: Specified artifact type not found')
+end
 
-% Plot the waveform of the considered channel
-figure
-subplot(2,1,1)
-plot(t,channel)         
-title('EEG data channel 3')
-xlabel('Time (s)')
-ylabel('Amplitude')
 
-NFFT = 2^nextpow2(L);               % Next power of 2 from length of the signal
-channel_dft = fft(channel,NFFT)/L;  % Fourier transform
-f = Fs/2*linspace(0,1,NFFT/2+1);    % Frequency vector
-
-% Plot the spectrum of the considered channel
-subplot(2,1,2)
-plot(f,2*abs(channel_dft(1:NFFT/2+1))) 
-title('Single-Sided Amplitude Spectrum of EEG channel 3')
-xlabel('Frequency (Hz)')
-ylabel('Amplitude')

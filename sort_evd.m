3ea55832ac94e35599db569c7d424723366834cf
Ben Somers <ben.somers@med.kuleuven.be>
2016-12-13 16:40:14 +0100

    make evd_sort do sorting in descending order

--- sort_evd.m
+++ sort_evd.m
@@ -1,6 +1,10 @@
 function [ eigvect_out, eigval_out ] = sort_evd(eigvect_in, eigval_in)
+% Sort a set of eigenvectors and eigenvalues in descending order
+%
+% The eigenvectors and eigenvalues are permutated in the same way to ensure
+% corresponding eigenvalues/eigenvectors correspond in the sorted result
 
-[~, permutation] = sort(diag(eigval_in));
+[~, permutation] = sort(diag(eigval_in),'descend');
 eigvect_out = eigvect_in(:,permutation);
 eigval_out = eigval_in(permutation,permutation);
 

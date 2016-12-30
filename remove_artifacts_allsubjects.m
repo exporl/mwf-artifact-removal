3844cccd4b137a6944dbdb985f17aafdffa8f673
Ben Somers <ben.somers@med.kuleuven.be>
2016-12-30 17:25:50 +0200

    created function to process all measuremnts of same artifact type for all subjects

---
+++ remove_artifacts_allsubjects.m
@@ -0,0 +1,16 @@
+% Process all files for a certain artifact type (e.g. all eye blink
+% measurements for all subjects)
+% 
+% Available artifact types for given dataset are:
+% 'eyeblink', 'muscle' ,'speech', 'movement', 'mix'
+
+function [ SER ,ARR ] = remove_artifacts_allsubjects(artifact_type)
+
+N_data = 10;
+SER = zeros(N_data, 1);
+ARR = zeros(N_data, 1);
+
+for i = 1 : N_data;
+    [~ ,~ ,~ ,SER(i), ARR(i)] = remove_artifacts(i, artifact_type);
+end
+

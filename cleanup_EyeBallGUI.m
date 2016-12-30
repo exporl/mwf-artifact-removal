289fa6fbb8e803f815ca12978fe173f9cc0029e3
Ben Somers <ben.somers@med.kuleuven.be>
2016-12-30 15:51:50 +0200

    rename and improve function for cleaning up temp variables

--- cleanup_EyeBallGUI.m
+++ cleanup_EyeBallGUI.m
@@ -1,20 +1,20 @@
 % Remove generated backup and initialization files created by EyeBallGUI 
 % in the current working directory.
 
-function cleanup
+function cleanup_EyeBallGUI
 
-if exist('EyeBallGUI_BackUp')
+if exist('EyeBallGUI_BackUp','dir')
     rmdir('EyeBallGUI_BackUp','s')
 end
 
-if exist('EyeBallGUIini.mat')
+if exist('EyeBallGUIini.mat','file')
     delete('EyeBallGUIini.mat')
 end
 
-if exist('TRAINING_DATA.mat')
+if exist('TRAINING_DATA.mat','file')
     delete('TRAINING_DATA.mat');
 end
 
-if exist('TRAINING_DATABad.mat')
+if exist('TRAINING_DATABad.mat','file')
     delete('TRAINING_DATABad.mat');
 end

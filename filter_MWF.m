55ea0cddd69117b1b900bac702f6c5f21196fd1c
Ben Somers <ben.somers@med.kuleuven.be>
2016-12-30 14:57:51 +0200

    put all time-lagging and GEVD functionality of MWF in one function

--- filter_MWF.m
+++ filter_MWF.m
@@ -1,40 +1,55 @@
 
-% TODO comments info
+% Multi-channel Wiener Filter (MWF) based on Generalized Eigenvalue
+% Decompostion (GEVD) for EEG artifact removal.
 
-function [v, d] = filter_MWF(y, mask)
+function [v, d] = filter_MWF(y, mask, delay)
 
-% Set parameters
-M = size(y,1);  % number of channels
+if (nargin < 3);
+    delay = 0;
+end
+
+% Introduce time lags
+M = size(y,1);
+M_s = (2*delay+1)*M;
+y_s = zeros(M_s,size(y,2));
+
+for tau = -delay:delay;
+    y_shift = circshift(y, tau, 2);
+    y_shift(:, [1:tau, end+tau+1:end]) = 0;
+    y_s((tau+delay)*M+1 : M*(tau+delay+1) , :) = y_shift;
+end
 
 % Calculate the covariance matrices Ryy and Rvv
-Ryy = cov(y.');
-Rvv = cov(y(:,mask == 0).');  % Rvv only uses clean data
-
-% Calculate the filter to estimate artifacts
+Ryy = cov(y_s.');
+Rvv = cov(y_s(:,mask == 0).');  % Rvv only uses clean data
 
 % normal MWF
-w = eye(M) - Ryy \ Rvv;
-
-% PE MWF == GEVD with threshold 0
-[v,d] = eig(w);
-[v,d] = sort_evd(v,d);
-d(d<0) = 0;
-w_pe = v*d/v;
+% w = eye(M) - Ryy \ Rvv;
 
 % GEVD-based MWF
-threshold = 0; %select largest eigenvalue to maintain threshold
 [X, GEVL] = eig(Ryy,Rvv);
-delta = GEVL - eye(M);
-delta(delta<threshold) = 0;
-w_gevd = X / GEVL * delta / X; 
+[X, GEVL] = sort_evd(X,GEVL);
+delta = GEVL - eye(M_s);
+
+% set filter rank
+% rank_w = M_s;                      % equivalent to normal MWF
+% rank_w = M_s - sum(diag(delta)<0); % equivalent to normal MWF, keep only positive EVs
+rank_w = M_s;
+
+% Create filter of rank rank_w
+delta(rank_w * (M_s + 1) + 1 : M_s + 1 : M_s * M_s) = 0; % set rank+1:end to 0
+w = X / GEVL * delta / X; 
 
 % Check assumption that X.' * Rvv * X is (close to) identity matrix
-if max(abs(diag(X.' * Rvv * X - eye(M))) > 10e-10) 
+if max(abs(diag(X.' * Rvv * X - eye(M_s))) > 10e-10) 
     error('Scaling error: assumption of scaling of generalized eigenvectors is not valid')
 end
 
 % subtract the eye blinks from training data
-d = (w.') * y;
-v = y - d;
+d_s = (w.') * y_s;
+v_s = y_s - d_s;
+
+d = d_s((delay)*M+1:M*(delay+1),:);
+v = v_s((delay)*M+1:M*(delay+1),:);
 
 end

640d3bfefd946393d9b2667e4fd931e3225a6f31
Ben Somers <ben.somers@med.kuleuven.be>
2016-12-30 15:46:10 +0200

    improved filter_performance: vectorized code instead of loops, remove redundant operations, documentation

--- filter_performance.m
+++ filter_performance.m
@@ -1,40 +1,38 @@
-function [SER, ARR] = filter_performance(y,d,blinks)
+function [SER, ARR] = filter_performance(y, d, mask)
 
-% PERFORMANCE takes a set of eeg data (y), and the artifacts estimates for
-% that set (d), and computes the performance parameters SER and SEAC based
-% on those. Furthermore, the function needs a single channel (blink_segments)
-% which indicates the artifact free- and corrupted regions.
-
-M = size(y,1); % number of channels
-
-% Separation of data (0 = no artifacts, 1 = corrupted)
-clean_d = d(:,blinks==0);
-clean_y = y(:,blinks==0);
-dirty_d = d(:,blinks==1);
-dirty_y = y(:,blinks==1);
-
-p = var(dirty_y.')-var(clean_y.');
-p(p < 0) = 0;
+% filter_performance takes multichannel eeg data y, and the multichannel 
+% artifact estimate d, and computes the performance parameters SER and ARR 
+% based on those. Furthermore, the function needs the artifact mask to 
+% compute the clean and corrupted regions.
 
 % SER = Signal-to-Error Ratio
-% Measured in parts without artifacts
-% WEIGHTED ACCORDING TO ARTIFACT POWER
-
-SER = 0;
-for j = 1:M
-    SER = SER + p(j)*10*log10( var(clean_y(j,:))/var(clean_d(j,:)) );
-end
-SER = SER/sum(p);
+% Measures alteration of EEG signal in parts without artifact
 
 % ARR = Artifact-to-Residu Ratio
-% Measured in parts with artifacts
-% WEIGHTED ACCORDING TO ARTIFACT POWER
+% Measures artifact estimate in parts with artifacts
 
-ARR = 0;
-for j = 1:M;
-    ARR = ARR + p(j)*10*log10( var(dirty_y(j,:))/var(dirty_y(j,:)-dirty_d(j,:)) );
-end
-ARR = ARR/sum(p);
+% Good artifact removal is indicated by high SER and high ARR
+
+% Segmentation of data using mask
+y_clean     = y(:, mask==0).';
+y_corrupt   = y(:, mask==1).';
+d_clean     = d(:, mask==0).';
+d_corrupt   = d(:, mask==1).';
+
+% Compute weights [0..1] according to artifact power per channel
+p = var(y_corrupt) - var(y_clean);
+p(p < 0) = 0;
+p = p / sum(p);
+
+% SER
+SER_pc = 10*log10(var(y_clean) ./ var(d_clean)); % SER per channel
+SER_w = SER_pc .* p; % SER per channel (weighted)
+SER = sum(SER_w); % Total SER (weighted average)
+
+% ARR
+ARR_pc = 10*log10(var(y_corrupt) ./ var(y_corrupt - d_corrupt)); % ARR per channel
+ARR_w = ARR_pc .* p; % ARR per channel (weighted)
+ARR = sum(ARR_w); % Total ARR (weighted average)
 
 end
 

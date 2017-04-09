6e70e955ba1bc0dd5c0fc9abeb4968d7a9e56d49
Ben Somers <ben.somers@med.kuleuven.be>
2017-04-10 00:09:07 +0200

    don't track localsettings

--- mwfgui_localsettings.m
+++
@@ -1,5 +0,0 @@
-% Some system-specific path setting
-function settings = mwfgui_localsettings
-settings = struct;
-settings.figurepath = 'C:\Users\gebruiker\Documents\KULeuven Doctoraat\Thesispaper 2 - GUI\Matlab - Sync\paper_figures\pf_figures';
-end

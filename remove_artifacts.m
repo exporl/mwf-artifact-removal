0778a383f8a4b5e6daa8dccfd3adca59406bb26c
Ben Somers <ben.somers@med.kuleuven.be>
2017-08-27 12:47:53 +0200

    comments for MWF artifact removal functions

--- remove_artifacts.m
+++ remove_artifacts.m
@@ -1,5 +1,21 @@
-% Remove specified artifact type 'artifact' from EEG measurement of subject
-% 'name'. Returns data y, artifact estimate d, clean data v, SER and ARR.
+% Use MWF to process EEG data corresponding to the given name/artifact 
+% inputs with the given processing parameters. 
+%
+% INPUTS:
+%   name        subject identifier (string)
+%   artifact    artifact type specifier (string)
+%   params      MWF parameter struct
+%
+% OUTPUTS: 
+%   y       raw EEG data (channels x samples)
+%   d       estimated artifacts in every channel (channels x samples)
+%   n       filtered EEG data (channels x samples)
+%   W       multi-channel Wiener filter (size depends on number of delays)
+%   SER     Signal to Error Ratio, measures clean EEG distortion
+%   ARR     Artifact to Residue Ratio, measures artifact estimation
+%
+% Author: Ben Somers, KU Leuven, Department of Neurosciences, ExpORL
+% Correspondence: ben.somers@med.kuleuven.be
 
 function [y, d, n, W, SER, ARR] = remove_artifacts(name, artifact, params)
 
--- remove_artifacts_allsubjects.m
+++ remove_artifacts_allsubjects.m
@@ -1,10 +1,20 @@
-% Process all files for a certain artifact type (e.g. all eye blink
-% measurements for all subjects)
-% 
-% Available artifact types for given dataset are:
-% 'eyeblink', 'muscle' ,'speech', 'movement', 'mix'
+% Use MWF to process EEG data corresponding to the given artifact input 
+% with the given processing parameters, for all subjects sequentially. 
+%
+% INPUTS:
+%   artifact    artifact type specifier (string)
+%   params      MWF parameter struct
+%
+% OUTPUTS: 
+%   SER     Signal to Error Ratio, measures clean EEG distortion, per subject
+%   ARR     Artifact to Residue Ratio, measures artifact estimation, per subject
+%   t       computation time (seconds), per subject
+%   pctkept  percentage of eigenvalues retained, per subject
+%
+% Author: Ben Somers, KU Leuven, Department of Neurosciences, ExpORL
+% Correspondence: ben.somers@med.kuleuven.be
 
-function [SER, ARR, t, pctkept] = remove_artifacts_allsubjects(artifact_type, params)
+function [SER, ARR, t, pctkept] = remove_artifacts_allsubjects(artifact, params)
 
 N_data = 10;
 SER = zeros(N_data, 1);
@@ -13,11 +23,11 @@ t = zeros(N_data, 1);
 pctkept = zeros(N_data, 1);
 
 for i = 1 : N_data;
-    if (strcmp(artifact_type,'eyeblink') && (i == 8))
+    if (strcmp(artifact,'eyeblink') && (i == 8))
         continue
     end
     tic
-    [~ ,~ ,~ , W, SER(i), ARR(i)] = remove_artifacts(i, artifact_type, params);
+    [~ ,~ ,~ , W, SER(i), ARR(i)] = remove_artifacts(i, artifact, params);
     t(i) = toc;
     pctkept(i) = rank(W)/size(W,1);
 end

8c3f17805574cc42b46898589aefcbfc43ef8bfa
Ben Somers <ben.somers@med.kuleuven.be>
2017-08-27 12:24:51 +0200

    initialize empty component cache if none exists yet

--- method_cached_components.m
+++ method_cached_components.m
@@ -2,11 +2,27 @@
 % type, subject and SNR. If they exist, return them. Is they don't exist,
 % return an empty array.
 %
-% If there is an additional input for components, the function stores those
-% components in the cache instead of checking for existance. Components
-% that were already cached will be overwritten.
+% If there are two input arguments, the function stores the components
+% specifed in the second input argument in the cache instead of checking 
+% for existance. Components that were already cached will be overwritten.
+%
+% INPUTS:
+%   id              data identifier struct
+%   newcomponents   [optional] new components to store in cache (vector)
+%
+% OUTPUTS: 
+%   components      components found in the cache, empty if nothing is
+%                   cached for given id (vector)
+%
+% Author: Ben Somers, KU Leuven, Department of Neurosciences, ExpORL
+% Correspondence: ben.somers@med.kuleuven.be
 
 function [components] = method_cached_components(id, newcomponents)
+
+% initialize component cache if needed
+if ~exist('method_cached_components.mat','file')
+    initialize_component_cache;
+end
 
 % ensure right field order
 id = orderfields(id); id = orderfields(id, [2, 1, 3, 4]);
@@ -14,18 +30,18 @@ id = orderfields(id); id = orderfields(id, [2, 1, 3, 4]);
 subfields = fieldnames(id);
 assert(isequal(subfields, {'method';'artifact';'name';'snr'}));
 
-exist = 0;
+entry_exists = 0;
 L = load('method_cached_components.mat');
 cached_components = L.cached_components;
 if check_cache(cached_components, id)
     components = cached_components.(id.method).(id.artifact).(id.name).(id.snr);
-    exist = true;
+    entry_exists = true;
 else
     components = [];
 end
 
 if nargin > 1
-    if exist
+    if entry_exists
         fprintf('Overwriting existing cached components... \n');
     end
     cached_components.(id.method).(id.artifact).(id.name).(id.snr) = newcomponents;
@@ -59,4 +75,10 @@ for j = 1:numel(subfields)
     end
 end
 
+end
+
+function initialize_component_cache
+    fprintf('Initializing new cache for components... \n');
+    cached_components = struct;
+    save('method_cached_components_test.mat', 'cached_components');
 end

b304e2102c28b0f3abd6ff1c0a730376d7a0e84f
Ben Somers <ben.somers@med.kuleuven.be>
2017-11-06 19:31:17 +0100

    standalone mask generating function

--- filter_getmask.m
+++ filter_getmask.m
@@ -1,17 +1,19 @@
-% Retrieve an artifact mask for the EEG data corresponding to the given
-% name/artifact inputs. If no mask is present or the "redo" flag is set, a
-% new mask will be created and stored in the cache.
+% Create an artifact mask for the input EEG data.
+% 
+% If an ID corresponding to a mask in the cache is provided, the cached 
+% mask will be loaded instead. If an ID is given and the "redo" flag is
+% set, the saved mask in the cache will be overwritten.
 %
 % The cache directory to read/write is set by mwfgui_localsettings.m
 %
 % INPUTS:
-%   name        subject identifier (string)
-%   artifact    artifact type specifier (string)
-%   redo        flag:   if 1, a new mask will always be generated
-%                       if 0, a new mask is generated only if no cache is present
-%   mode        GUI/toolbox to use for artifact marking
-%                       0: EyeBallGUI
-%                       1: EEGLAB (eegplot)
+%   y       raw EEG data (channels x samples)
+%   fs      EEG data sample rate
+%   redo    flag:   if 1, a new mask will always be generated
+%                   if 0, a new mask is generated only if no cache is present
+%   mode    GUI/toolbox to use for artifact marking
+%               0: EEGLAB (eegplot)
+%               1: EyeBallGUI
 %
 % OUTPUTS: 
 %   mask    markings of artifacts in y (1 x samples)
@@ -24,7 +26,7 @@
 % Author: Ben Somers, KU Leuven, Department of Neurosciences, ExpORL
 % Correspondence: ben.somers@med.kuleuven.be
 
-function [mask] = get_artifact_mask(name, artifact, redo, mode)
+function [mask] = filter_getmask(y, fs, redo, mode)
 
 if (nargin <3)
     redo = 0; % default: attempt to load existing mask from cache
@@ -33,44 +35,12 @@ if (nargin < 4)
     mode = 1; % default: EEGLAB
 end
 
-if (isa(name,'double'))
-    name = get_name(name);
-end
-
 settings = mwfgui_localsettings;
 maskpath = fullfile(settings.savemaskpath,[name '_' artifact '_mask.mat']);
 
 if (~exist(maskpath, 'file') || redo)
-    [FileEEGdata, FileEEGsrate, ~] = get_data(name, artifact);
-    if (~mode) % Use EyeBallGUI
-        % Create .mat file to be loaded by EyeBallGUI
-        save('TRAINING_DATA.mat','FileEEGdata','FileEEGsrate')
-    
-        % Launch EyeBallGUI
-        EyeBallGUI
-        % Wait until the "Bad" file exists with markings in the directory. If one
-        % exists already, delete it
-        if(exist('TRAINING_DATABad.mat','file'))
-            delete('TRAINING_DATABad.mat');
-        end
-        while(~exist('TRAINING_DATABad.mat','file'))
-            pause(1);
-        end
-
-        % Close GUI after markings are saved and clean up backup files
-        close all force
-        rmdir('EyeBallGUI_BackUp','s')
-        delete('EyeBallGUIini.mat')
-
-        % Extract marked artifact segments from markings .mat file
-        load('TRAINING_DATABad.mat');
-        mask = isnan(EEGmask);
-        mask = sum(mask,1);
-        mask(mask>0) = 1;
-        
-        cleanup_EyeBallGUI;
-    else % Use EEGLAB
-        eegplot(FileEEGdata, 'srate', FileEEGsrate, 'winlength', 10, ...
+    if (~mode) % Use EEGlab
+        eegplot(y, 'srate', fs, 'winlength', 10, ...
             'tag', 'eegplot_marks', 'command', 'get_mask_eeglab', 'butlabel', 'SAVE MARKS');
         h = findobj(0, 'tag', 'eegplot_marks');
         [FIGexist, TMPREJexist] = updateEegplotStatus(h);
@@ -98,10 +68,37 @@ if (~exist(maskpath, 'file') || redo)
         evalin('base',['clear ', 'TMPREJ']) %clear global
         
         %build mask from markings
-        mask = zeros(1,size(FileEEGdata,2));
+        mask = zeros(1,size(y,2));
         for i = 1:size(markings,1)
             mask(:,floor(markings(i,1)):ceil(markings(i,2))) = 1;
         end        
+    else % use EyeBallGUI
+        % Create .mat file to be loaded by EyeBallGUI
+        save('TRAINING_DATA.mat','FileEEGdata','FileEEGsrate')
+    
+        % Launch EyeBallGUI
+        EyeBallGUI
+        % Wait until the "Bad" file exists with markings in the directory. If one
+        % exists already, delete it
+        if(exist('TRAINING_DATABad.mat','file'))
+            delete('TRAINING_DATABad.mat');
+        end
+        while(~exist('TRAINING_DATABad.mat','file'))
+            pause(1);
+        end
+
+        % Close GUI after markings are saved and clean up backup files
+        close all force
+        rmdir('EyeBallGUI_BackUp','s')
+        delete('EyeBallGUIini.mat')
+
+        % Extract marked artifact segments from markings .mat file
+        load('TRAINING_DATABad.mat');
+        mask = isnan(EEGmask);
+        mask = sum(mask,1);
+        mask(mask>0) = 1;
+        
+        cleanup_EyeBallGUI; 
     end
     save(maskpath, 'mask');
 else % mask already exists

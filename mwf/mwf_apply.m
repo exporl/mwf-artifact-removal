9e7f1d0724476ae07ab6da44e88007cd15ee2d48
Ben Somers <ben.somers@med.kuleuven.be>
2018-02-09 00:38:15 +0100

    allow filtering of EEG with DC component: added mean subtraction

--- mwf/mwf_apply.m
+++ mwf/mwf_apply.m
@@ -1,6 +1,11 @@
 % Apply a pre-computed Multi-channel Wiener Filter W on a multi-channel EEG
 % signal y, producing artifact estimate d and filtered signal n.
 %
+% This function works for input EEG with a DC component. As the MWF is 
+% computed on zero-meaned data, it should be applied on zero-meaned
+% data as well. Therefore, the mean of the EEG data is subtracted first
+% and added after MWF application. 
+%
 % INPUTS:
 %   y       raw EEG data (channels x samples)
 %   W       precomputed multi-channel Wiener filter
@@ -14,13 +19,17 @@
 
 function [n, d] = mwf_apply(y, W)
 
-M = size(y, 1);
+[M, T] = size(y);
 M_s = size(W, 1);
 
 tau = (M_s - M) / (2 * M);
 if mod(tau, 1) ~= 0
     error('the given filter is not compatible with the input EEG signal')
 end
+
+% subtract mean from data
+channelmeans = mean(y,2);
+y = y - repmat(channelmeans, 1, T);
 
 % re-apply time lags to y to apply filter W
 [y_s, ~] = mwf_utils.stack_delay_data(y, tau);
@@ -31,5 +40,8 @@ d = W(:, orig_chans).' * y_s;
 
 % subtract artifact estimate from data
 n = y - d;
+
+% add mean back to filtered EEG
+n = n + repmat(channelmeans, 1, T);
 
 end
